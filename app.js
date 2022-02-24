3738923a7092bedc4752ec041f741a0864a92ae3
louzanizineddine <louzanizineddine@gmail.com>
2022-02-24 18:26:47 +0100

    finishde the matrixGame class

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 class MatrixGame {
     constructor() {
         this.level = 5;
-        this.score = 0;
         this.clickedSquares = [];
         this.correctSquares = [];
-        this.allowUserToclick = true;
+        this.allowUserToclick = false;
         this.gameDom = document.querySelector('.game');
+        this.playAgainBtn = document.querySelector('.play-again');
+        this.nextLevelBtn = document.querySelector('.next-level');
     }
-
+    // a function to build the matrix 
     fillMatrix(level) {
         while(this.correctSquares.length < level){
             let randomNumber = Math.floor(Math.random() * Math.pow(level + 2 , 2));
@@ -17,9 +18,12 @@ class MatrixGame {
             console.log(document.querySelector(`.cell-${el}`))
             document.querySelector(`.cell-${el}`).classList.add('active');
         })
-
-        setTimeout(this.hideColorfulSquares  , 3000)
+        // setTimeout( () => {
+        //     this.hideColorfulSquares()
+        // }  , 3000);
     }
+
+    // we hide the colorful squres 
 
     hideColorfulSquares() {
         let colorfulSquares = document.querySelectorAll('.active');
@@ -27,19 +31,81 @@ class MatrixGame {
             square.classList.remove('active');
         })
         this.allowUserToclick = true;
-        console.log(this.gameDom , this.level)
-        this.gameDom.addEventListener('click' , (e) => {
-            if(this.allowUserToclick && (this.clickedSquares.length < this.level)) this.userChoice(e)
-        });
+        // if (this.allowUserToclick){
+        //     this.userChoice()
+        // }
     }
 
-    userChoice(event) {
-               event.target.classList.add('clicked-square')
-                this.clickedSquares.push(event.target)
-                console.log(this.clickedSquares)
+    userChoice() {
+        this.gameDom.addEventListener(('click') , (e) => {
+            if (this.clickedSquares.length < this.level) {
+                e.preventDefault();
+                e.target.classList.add('clicked-square');
+                this.clickedSquares.push(Number(e.target.dataset.indexNumber));
+            } else {
+                this.evaluateUserClicks()
+            }
+        })
+    }
+
+    evaluateUserClicks() {
+        for (let elem of this.clickedSquares) {
+            console.log(elem)
+            if (this.correctSquares.includes(elem)) {
+                this.showCorrectAnswer(elem)
+            } else {
+                this.showWrongAnswer(elem)
+            }
+        } 
+        for (let elem of this.correctSquares) {
+            this.showCorrectAnswer(elem);
+        }
+
+        this.updateLevel();
+    }
+
+    updateLevel() {
+        if (document.querySelectorAll('correct').length === this.level) {
+            this.level++;
+            this.nextLevelBtn.classList.remove('d-none');
+            this.nextLevelBtn.addEventListener('click' , (e) => {
+                e.preventDefault();
+                this.nextLevelBtn.classList.add('d-none');
+                this.clearTheGame()
+                // this.buildMatrix(this.level)
+            })
+        }   
+
+        else {
+            this.playAgainBtn.classList.remove('d-none');
+            this.playAgainBtn.addEventListener('click' , (e) => {
+                e.preventDefault();
+                this.playAgainBtn.classList.add('d-none');
+                this.clearTheGame()
+                // this.buildMatrix(this.level)
+            })
+        }
+    }
+
+    clearTheGame() {
+        while (this.gameDom.firstChild) {
+            this.gameDom.firstChild.remove()
+            console.log('something node is removed')
+        }
+    }
+
+    showWrongAnswer(index) {
+        const squareElem = document.querySelector(`.cell-${index}`);
+        squareElem.classList.add('wrong');
+    }
+
+    showCorrectAnswer(index) {
+        const squareElem = document.querySelector(`.cell-${index}`);
+        squareElem.classList.add('correct');
     }
 
     buildMatrix(level) {
+        console.log('hey am been called again')
         let index = 1;
         for (let i = 0; i < level + 2; i++) {
             const row = document.createElement('div');
@@ -49,14 +115,23 @@ class MatrixGame {
                 const cell = document.createElement('div');
                 cell.className = 'cell'
                 cell.classList.add(`cell-${index}`);
+                cell.dataset.indexNumber = index;
                 // cell.textContent = `${index}`;
                 row.appendChild(cell);
                 index++;
             }
         }
-        this.fillMatrix(this.level)
+        // this.fillMatrix(this.level)
     }
 }
 
 const a = new MatrixGame()
 a.buildMatrix(a.level)
+a.fillMatrix(a.level)
+setTimeout( () => {
+    this.hideColorfulSquares()
+}  , 3000);
+
+if (a.allowUserToclick){
+    a.userChoice()
+}
